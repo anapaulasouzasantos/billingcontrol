b86cdd11bd24b77bdd9c451e142646e72a086ebe
githubdaAna <kristinnacruzz@gmail.com>
2023-04-15 19:31:15 -0300

    feat: component CardClientSummary

--- desafio-m05/src/config/router.jsx
+++ desafio-m05/src/config/router.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes, Outlet, Navigate } from 'react-router-dom'
 import SignIn from '../pages/SignIn';
-// import SignUp from '../pages/SignUp';
+import Teste from '../pages/Teste';
 import Main from '../pages/Main';
 
 import { getItem } from '../functions/storage'
@@ -14,8 +14,8 @@ function Router() {
     return (
         <Routes>
             <Route path='/' element={<SignIn />} />
-            {/* <Route path='/sign-up' element={<SignUp />} /> */}
-                <Route path='/main' element={<Main />} />
+            <Route path='/teste' element={<Teste/>} />
+            <Route path='/main' element={<Main />} />
             <Route element={<ProtectedRoutes redirectTo='/' />} >
             </Route>
         </Routes>
