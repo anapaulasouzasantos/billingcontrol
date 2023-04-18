094a0ae4c4c78b2bfe54738f598676f700ec0a4f
EduardoRag <eduardojarek66@gmail.com>
2023-04-18 10:18:42 -0300

    Ajustes no sidebar e no header

--- desafio-m05/src/components/Sidebar/index.jsx
+++ desafio-m05/src/components/Sidebar/index.jsx
@@ -1,31 +1,67 @@
 import './styles.css';
 import HomeIcon from '../../assets/home-icon.svg';
+import SelectedHomeIcon from '../../assets/selected-home.svg';
 import ClientsIcon from '../../assets/clients-icon.svg';
+import SelectedClientsIcon from '../../assets/selected-clients.svg';
 import ChargeIcon from '../../assets/charge-icon.svg';
+import { useState } from 'react';
 
 function Sidebar() {
+    const [selectHomeIcon, setSelectHomeIcon] = useState(true);
+    const [selectClientsIcon, setSelectClientsIcon] = useState(false);
+    const [selectChargeIcon, setSelectChargeIcon] = useState(false);
+
+    const handleClickHomeIcon = () => {
+        setSelectHomeIcon(true);
+        setSelectClientsIcon(false);
+        setSelectChargeIcon(false);
+    }
+
+    const handleClickClientsIcon = () => {
+        setSelectHomeIcon(false);
+        setSelectClientsIcon(true);
+        setSelectChargeIcon(false);
+    }
+
+    const handleClickChargeIcon = () => {
+        setSelectHomeIcon(false);
+        setSelectClientsIcon(false);
+        setSelectChargeIcon(true);
+    }
     return (
         <aside>
-            <button>
+            <button
+                className={selectHomeIcon && 'selected'}
+                onClick={handleClickHomeIcon}
+            >
                 <img
-                    src={HomeIcon}
+                    src={selectHomeIcon ? SelectedHomeIcon : HomeIcon}
                     alt='Home Icon'
                 />
-                <strong>Home</strong>
+                <strong>
+                    Home
+                </strong>
             </button>
-            <button>
+            <button
+                className={selectClientsIcon && 'selected'}
+                onClick={handleClickClientsIcon}
+            >
                 <img
-                    src={ClientsIcon}
+                    src={selectClientsIcon ? SelectedClientsIcon : ClientsIcon}
                     alt='Clients Icon'
                 />
-                <strong>Clientes</strong>
+                <strong>
+                    Clientes
+                </strong>
             </button>
             <button>
                 <img
                     src={ChargeIcon}
                     alt='Charge Icon'
                 />
-                <strong>Cobranças</strong>
+                <strong>
+                    Cobranças
+                </strong>
             </button>
         </aside>
     )
