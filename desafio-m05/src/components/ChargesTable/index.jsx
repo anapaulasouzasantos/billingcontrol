e5cabe204bcf3ecb8dfeacf53497e1b103db856a
githubdaAna <kristinnacruzz@gmail.com>
2023-04-24 09:35:57 -0300

    feat: page clients detail

---
+++ desafio-m05/src/components/ChargesTable/index.jsx
@@ -0,0 +1,75 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import UpDownArrow from '../../assets/updown-arrows.svg';
+import Edit from '../../assets/edit-profile.svg';
+import Delete from '../../assets/delete-icon.svg';
+import './styles.css';
+
+function createData(id, dueDate, value, status, description) {
+    return { id, dueDate, value, status, description };
+}
+
+const rows = [
+    createData('248563147', '26/01/2021', 'R$ 500,00', 'Vencida', 'lorem ipsum lorem ipsum lorem ipsuipsum lorem ips,,,'),
+    createData('248563147', '26/01/2021', 'R$ 500,00', 'Vencida', 'lorem ipsum lorem ipsum lorem ipsuipsum lorem ips,,,'),
+    createData('248563147', '26/01/2021', 'R$ 500,00', 'Vencida', 'lorem ipsum lorem ipsum lorem ipsuipsum lorem ips,,,'),
+];
+
+export default function AccessibleTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow >
+                        <TableCell
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}
+                            className='subtitle-charges-table'
+                            align="center" >
+                            <img className='up-down-icon' src={UpDownArrow} />
+                            ID Cob.</TableCell>
+                        <TableCell
+                            className='subtitle-charges-table'
+                            align="center">
+                            <img className='up-down-icon' src={UpDownArrow} />
+                            Data de Venc.</TableCell>
+                        <TableCell className='subtitle-charges-table' align="left">Valor</TableCell>
+                        <TableCell className='subtitle-charges-table' align="left">Status</TableCell>
+                        <TableCell className='subtitle-charges-table' align="left">Descrição</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.id}>
+                            <TableCell className='table-contents' component="th" scope="row">
+                                {row.id}
+                            </TableCell>
+                            <TableCell className='table-contents' align="center">{row.dueDate}</TableCell>
+                            <TableCell className='table-contents' align="left">{row.value}</TableCell>
+                            <TableCell
+                                className='table-contents'
+                                align="left"
+                            ><strong className='expired-status' >{row.status}</strong>
+                            </TableCell>
+                            <TableCell
+                                className='table-contents'
+                                align="left"
+                                sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                {row.description}
+                                <img src={Edit} />
+                                <img src={Delete} />
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer >
+    );
+}
--- desafio-m05/src/components/Content/ContentClientsDetail/index.jsx
+++ desafio-m05/src/components/Content/ContentClientsDetail/index.jsx
@@ -1,15 +1,18 @@
 import './styles.css';
-import ClientsIcon from '../../../assets/clients-icon.svg'
+import ClientsIcon from '../../../assets/clients-icon.svg';
+import CardCustomerCharges from '../../CardCustomerCharges';
 
 const ContentClientsDetail = () => {
   return (
     <div className='container-content-clients-detail'>
-        <div>
-          <div>
-            <img src={ClientsIcon} alt='client icon'></img>
-            <h1>Sara Lage da Silva</h1>
-          </div>
-        </div>
+      <div className='top-info'>
+        <img src={ClientsIcon} alt='client icon'></img>
+        <h1>Sara Lage da Silva</h1>
+      </div>
+      <div className='card-client-info'>Card dados do cliente</div>
+      <div>
+        <CardCustomerCharges></CardCustomerCharges>
+      </div>
     </div>
   )
 }
